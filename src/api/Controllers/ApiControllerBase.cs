5696f1cca3ee3f9e316239463058e094b7ae9f44
Maxime Labelle <springcomp@users.noreply.github.com>
2021-12-18 12:58:36 +0100

    [api] Added profiles controller.

---
+++ src/api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Claims;
+
+public class ApiControllerBase
+{
+    protected bool GetAuthenticatedUserId(ClaimsPrincipal identity, out string? identifier)
+    {
+        identifier = null;
+
+        var claim = identity
+            .Claims
+            .Where(c => c.Type == ClaimTypes.Name)
+            .FirstOrDefault()
+            ;
+
+        if (claim == null) return false;
+
+        identifier = claim.Value;
+        identifier = "a1118e83-92e6-4465-91e1-7595d060195c";
+        return true;
+    }
+
+    protected static IActionResult BadRequest()
+    {
+        return new StatusCodeResult(StatusCodes.Status400BadRequest);
+    }
+    protected static IActionResult Conflict()
+    {
+        return new StatusCodeResult(StatusCodes.Status409Conflict);
+    }
+    protected static IActionResult NoContent()
+    {
+        return new StatusCodeResult(StatusCodes.Status204NoContent);
+    }
+    protected static IActionResult Ok()
+    {
+        return new OkResult();
+    }
+    protected static IActionResult Ok<T>(T @object)
+    {
+        return new OkObjectResult(@object);
+    }
+
+    protected static T GetQueryParameter<T>(HttpRequest req, string name, T defaultValue)
+    {
+        return  GetQueryParameter<T>(req, name) ?? defaultValue;
+    }
+    protected static T? GetQueryParameter<T>(HttpRequest req, string name)
+    {
+        var text = req.Query[name].FirstOrDefault();
+        if (text != null){
+            if (typeof(T) == typeof(int)){
+                if (Int32.TryParse(text, out int _number))
+                    return (T)(object)_number;
+            }
+            return (T?)(object?)null;
+        }
+
+        return (T?)(object?)null;
+    }
+    protected static string? GetQueryParameter(HttpRequest req, string name)
+    {
+        return req.Query[name].FirstOrDefault();
+    }
+    protected static string GetQueryParameter(HttpRequest req, string name, string defaultValue)
+    {
+        return req.Query[name].FirstOrDefault() ?? defaultValue;
+    }
+}
---
+++ src/api/Controllers/ProfilesController.cs
@@ -0,0 +1,231 @@
+using MaskedEmails.Model;
+using MaskedEmails.Services.Interop;
+using System.Security.Claims;
+public sealed class ProfilesController : ApiControllerBase
+{
+	const string AUTOGENERATE_EMAIL = null;
+
+	private readonly IConfiguration configuration_;
+	private readonly IProfilesService service_;
+
+	public ProfilesController(
+		  IConfiguration configuration
+		, IProfilesService service
+	)
+	{
+		configuration_ = configuration;
+		service_ = service;
+	}
+
+	[FunctionName("get-profiles-my")]
+	public async Task<IActionResult> GetMyProfileAsync(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "profiles/my")] HttpRequest request,
+		[UserInfo] ClaimsPrincipal identity
+	)
+	{
+		if (!GetAuthenticatedUserId(identity, out var identifier))
+			return BadRequest();
+
+		var profile = await service_
+				.GetProfile(identifier)
+			;
+
+		return Ok(profile);
+	}
+
+	[FunctionName("patch-profiles-my")]
+	public async Task<IActionResult> UpdateUserProfileAsync(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "patch", Route = "profiles/my")] UpdateProfileRequest profile,
+		[UserInfo] ClaimsPrincipal identity
+	)
+	{
+		if (!GetAuthenticatedUserId(identity, out var identifier))
+			return BadRequest();
+
+		var updated = await service_
+			.UpdateProfile(identifier, profile.DisplayName, profile.ForwardingAddress);
+		;
+
+		return Ok(updated);
+	}
+
+	[FunctionName("post-profiles-my-addresses-email")]
+	public async Task<IActionResult> CreateMaskedEmailAsync(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "profiles/my/addresses")] CreateMaskedEmailRequest request,
+		[UserInfo] ClaimsPrincipal identity,
+		HttpRequest req
+	)
+	{
+		// the angular application does not pass a specific email address
+		// some other clients might want to specify a specific email address
+
+		string? email = AUTOGENERATE_EMAIL;
+		email = GetQueryParameter(req, email);
+
+		if (!GetAuthenticatedUserId(identity, out var identifier))
+			return BadRequest();
+
+		try
+		{
+
+			MaskedEmailWithPassword address = null;
+
+			if (email == AUTOGENERATE_EMAIL)
+			{
+				address = await service_
+						.CreateMaskedEmail(identifier,
+							request.Name,
+							request.PasswordHash,
+							request.Description,
+							request.EnableForwarding
+							);
+			}
+
+			else
+			{
+				address = await service_
+						.CreateMaskedEmail(identifier,
+							request.Name,
+							email,
+							request.PasswordHash,
+							request.Description,
+							request.EnableForwarding
+							);
+			}
+
+			// TODO: 201
+			return Ok(address);
+		}
+		catch (ArgumentException)
+		{
+			return Conflict();
+		}
+	}
+
+	[FunctionName("get-profiles-my-search")]
+	public async Task<IActionResult> SearchMaskedEmailsAsync(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "profiles/my/search")] HttpRequest request,
+		[UserInfo] ClaimsPrincipal identity
+	)
+	{
+		string? contains = GetQueryParameter(request, "contains");
+		int top = GetQueryParameter(request, "top", 25);
+		string? cursor = GetQueryParameter(request, "cursor");
+		string sort_by = GetQueryParameter(request, "sort_by", "created-utc-desc");
+
+		if (!GetAuthenticatedUserId(identity, out var identifier))
+			return BadRequest();
+
+		var page = await service_
+			.GetMaskedEmails(identifier, top, cursor, sort_by, search: contains);
+
+		return Ok(page);
+	}
+
+	[FunctionName("get-profiles-my-address-pages")]
+	public async Task<IActionResult> GetMaskedEmailsPagingAsync(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "profiles/my/address-pages")] HttpRequest request,
+		[UserInfo] ClaimsPrincipal identity
+	)
+	{
+		int top = GetQueryParameter(request, "top", 25);
+		string? cursor = GetQueryParameter(request, "cursor");
+		string sort_by = GetQueryParameter(request, "sort_by", "created-utc-desc");
+
+		if (!GetAuthenticatedUserId(identity, out var identifier))
+			return BadRequest();
+
+		var page = await service_
+			.GetMaskedEmails(identifier, top, cursor, sort_by, search: null);
+
+		return Ok(page);
+	}
+
+	[FunctionName("get-profiles-my-addresses")]
+	public async Task<IActionResult> GetMaskedEmailsAsync(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "profiles/my/addresses")] HttpRequest request,
+		[UserInfo] ClaimsPrincipal identity
+	)
+	{
+		if (!GetAuthenticatedUserId(identity, out var identifier))
+			return BadRequest();
+
+		var addresses = await service_
+				.GetMaskedEmails(identifier)
+			;
+
+		return Ok(addresses);
+	}
+
+	[FunctionName("get-profiles-my-addresses-email")]
+	public async Task<IActionResult> GetMaskedEmailAsync(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "profiles/my/addresses/{email}")] HttpRequest request,
+		[UserInfo] ClaimsPrincipal identity,
+		string email
+	)
+	{
+		if (!GetAuthenticatedUserId(identity, out var identifier))
+			return BadRequest();
+
+		var address = await service_
+				.GetMaskedEmail(identifier, email)
+			;
+
+		return Ok(address);
+	}
+
+	[FunctionName("patch-profiles-my-addresses-email-enableForwarding")]
+	public async Task<IActionResult> ToggleMaskedEmailForwardingAsync(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "patch", Route = "profiles/my/addresses/{email}/enableForwarding")] HttpRequest request,
+		[UserInfo] ClaimsPrincipal identity,
+		string email
+	)
+	{
+		if (!GetAuthenticatedUserId(identity, out var identifier))
+			return BadRequest();
+
+		var address = await service_
+				.ToggleMaskedEmailForwarding(identifier, email)
+			;
+
+		return Ok(address);
+	}
+
+	[FunctionName("patch-profiles-my-addresses-email")]
+	public async Task<IActionResult> UpdateMaskedEmailAsync(
+			[HttpTrigger(AuthorizationLevel.Anonymous, "patch", Route = "profiles/my/addresses/{email}")] UpdateMaskedEmailRequest request,
+			[UserInfo] ClaimsPrincipal identity,
+			string email
+		)
+	{
+		if (!GetAuthenticatedUserId(identity, out var identifier))
+			return BadRequest();
+
+		await service_
+				.UpdateMaskedEmail(identifier,
+					email,
+					request.Name,
+					request.Description,
+					request.PasswordHash
+					);
+
+		return Ok();
+	}
+
+	[FunctionName("delete-profiles-my-addresses-email")]
+	public async Task<IActionResult> DeleteMaskedEmailsAsync(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "profiles/my/addresses/{email}")] HttpRequest request,
+		[UserInfo] ClaimsPrincipal identity,
+        string email
+	)
+	{
+		if (!GetAuthenticatedUserId(identity, out var identifier))
+			return BadRequest();
+
+		await service_.DeleteMaskedEmail(identifier, email)
+			;
+
+		return NoContent();
+	}
+
+}
